56cf5d2c8f142d138e05dafad48087c73f719d5a
Kahlouche Omar Djaber <djabir.perso@outlook.fr>
2025-06-24 22:39:46 +0100

    44. Adding More Components to the Demo App

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,17 @@
 import { Component, computed, EventEmitter, Input, input, output, Output } from '@angular/core';
 
+// type User = {
+//     id :string;
+//     avatar :string;
+//     name :string;
+//   };
+
+interface User {
+  id :string;
+  avatar :string;
+  name :string;
+};
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,24 +20,15 @@ import { Component, computed, EventEmitter, Input, input, output, Output } from 
 })
 export class UserComponent {
 
-  @Input({required: true}) id!:string;
-  @Input({required: true }) avatar!: string;
-  @Input({required: true}) name!: string;
-
+  @Input({required: true}) user!: User;
+ 
   @Output() select = new EventEmitter<string>();
-  // select = output<string>();
   
   get imagePath(){
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   };
 
-  // avatar = input.required<string>();
-  // name = input.required<string>();
-  // imagePath = computed(() => {
-  //   return 'assets/users/' + this.avatar();
-  // });
-
   onSelectUser(){
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
