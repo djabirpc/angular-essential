98c41e9137a1e89053ed5f702f0a2694b326835a
Kahlouche Omar Djaber <djabir.pc@outlook.fr>
2025-06-27 18:30:36 +0100

    62. Using localStorage for Data Storage

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,27 +1,18 @@
 import { Component, computed, EventEmitter, Input, input, output, Output } from '@angular/core';
-
-// type User = {
-//     id :string;
-//     avatar :string;
-//     name :string;
-//   };
-
-interface User {
-  id :string;
-  avatar :string;
-  name :string;
-};
+import { type User } from './user.model';
+import { CardComponent } from "../shared/card/card.component";
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+  styleUrl: './user.component.css',
+  imports: [CardComponent]
 })
 export class UserComponent {
 
   @Input({required: true}) user!: User;
- 
+  @Input({required : true}) selected!: boolean;
   @Output() select = new EventEmitter<string>();
   
   get imagePath(){
